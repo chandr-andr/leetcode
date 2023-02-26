3ac480be23abbf1dc0b3dc1deb88bd3ec6f0a385
Aleksandr <askiselev00@gmail.com>
2023-02-26 23:28:26 +0400

    Solved task

---
+++ python/ Length_of_Last_Word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split()
+        return len(words[-1])
