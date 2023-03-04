9991e1566b284c7af5e1f693a4d59745fdab2019
Aleksandr Kiselev <aleksandrkiselev@Aleksandrs-MacBook-Pro.local>
2023-03-04 14:37:24 +0400

    solved task

---
+++ python/Combination_Sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        st = []
+
+        def dfs(candidates, target):
+            if target < 0:
+                return
+            if target == 0:
+                result.append(st.copy())
+                return
+            for candi_idx in range(len(candidates)):
+                st.append(candidates[candi_idx])
+                dfs(candidates[candi_idx:], target-candidates[candi_idx])
+                st.pop()
+
+        dfs(candidates, target)
+        return result
+    
+
+s = Solution()
+print(s.combinationSum([2,3,6,7], 7))
+print(s.combinationSum([2,3,5], 8))
---
+++ rust/target/debug/deps/ThreeSum-3ec64366b5a71980.d
@@ -0,0 +1,5 @@
+/Users/aleksandrkiselev/Projects/mygithub/leetcode/rust/target/debug/deps/ThreeSum-3ec64366b5a71980.rmeta: ThreeSum/src/main.rs
+
+/Users/aleksandrkiselev/Projects/mygithub/leetcode/rust/target/debug/deps/ThreeSum-3ec64366b5a71980.d: ThreeSum/src/main.rs
+
+ThreeSum/src/main.rs:
