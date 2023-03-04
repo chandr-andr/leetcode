3017739d9d47c63964bb90be5857c36a34f6455e
Aleksandr Kiselev <aleksandrkiselev@Aleksandrs-MacBook-Pro.local>
2023-03-04 14:37:53 +0400

    Removed rust target dir

--- rust/target/debug/deps/ThreeSum-a241de12ea3cb513.d
+++
@@ -1,5 +0,0 @@
-/Users/aleksandrkiselev/Projects/leetcode/rust/target/debug/deps/ThreeSum-a241de12ea3cb513.rmeta: ThreeSum/src/main.rs
-
-/Users/aleksandrkiselev/Projects/leetcode/rust/target/debug/deps/ThreeSum-a241de12ea3cb513.d: ThreeSum/src/main.rs
-
-ThreeSum/src/main.rs:
