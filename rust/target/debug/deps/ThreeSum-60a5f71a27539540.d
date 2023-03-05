cccf39c123583f65195801d275dd1821b862a7f9
Aleksandr <askiselev00@gmail.com>
2023-03-05 15:15:27 +0400

    solved task

--- rust/target/debug/deps/ThreeSum-60a5f71a27539540.d
+++
@@ -1,5 +0,0 @@
-/Users/aleksandrkiselev/Projects/leetcode/rust/target/debug/deps/ThreeSum-60a5f71a27539540.rmeta: ThreeSum/src/main.rs
-
-/Users/aleksandrkiselev/Projects/leetcode/rust/target/debug/deps/ThreeSum-60a5f71a27539540.d: ThreeSum/src/main.rs
-
-ThreeSum/src/main.rs:
